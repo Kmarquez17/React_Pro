4c68b7bdd2076801f91bcd79931ed2381177564a
Kevin Marquez <marquezkrodriguez24@gmail.com>
2022-05-10 21:34:09 -0600

    Configurando ts, con la libreria mapBox a la hora de transpilar el build

--- mi-app/src/index.tsx
+++ mi-app/src/index.tsx
@@ -1,7 +1,10 @@
+/* eslint import/no-webpack-loader-syntax: off */
 import React from "react";
 import ReactDOM from "react-dom";
 import  MapsApp from "./MapsApp";
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+//@ts-ignore
+import mapboxgl from '!mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
  
 mapboxgl.accessToken = 'pk.eyJ1Ijoia21hcnF1ZXoxNyIsImEiOiJja21kdTRxd3QyY2N0MnBzOXZncGhkNnR6In0.tAZZTSGr95xBzog-Vy0O-g';
 
