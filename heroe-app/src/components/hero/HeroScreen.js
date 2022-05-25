d690e8df4009d9fa691af020726bc338d3134572
Kevin Marquez <marquezkrodriguez24@gmail.com>
2022-05-24 19:31:27 -0600

    Memorizando peticiones que vienen como(Helpers), leer argumentos de la URL

--- heroe-app/src/components/hero/HeroScreen.js
+++ heroe-app/src/components/hero/HeroScreen.js
@@ -1,7 +1,61 @@
-export const HeroScreen = () => {
+import {useMemo} from 'react';
+import { Navigate,useNavigate, useParams } from "react-router-dom";
+import { getHeroById } from "../../selectors/getHeroById";
+
+export const HeroScreen = () => {  
+  const { id } = useParams();
+  const navigate = useNavigate()
+  
+  const hero = useMemo(() => getHeroById(id), [id]);
+
+  
+  const handleReturn = () => {
+    navigate( -1 );
+  };
+
+  if (!hero) return <Navigate to="/" />;
+
+  const {
+    id: idHero,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters,
+  } = hero;
+  
+  const imagePath = `/assets/${idHero}.jpg`;
   return (
-    <div>
-      <h1>HeroScreen</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={imagePath}
+          alt={superhero}
+          className="img-thumbnail animate__animated animate__fadeInLeft"
+        />
+      </div>
+
+      <div className="col-8 animate__animated animate__fadeIn">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego:</b> {alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher:</b> {publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First Appearance:</b> {first_appearance}
+          </li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{characters}</p>
+
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Regresar
+        </button>
+      </div>
     </div>
   );
 };
---
+++ heroe-app/src/selectors/getHeroesByPublisher.js
@@ -0,0 +1,11 @@
+import { heroes } from "../data/Heroes";
+
+export const getHeroesByPublisher = (publisher) => {
+  const validaPublisher = ["DC Comics", "Marvel Comics"];
+
+  if (!validaPublisher.includes(publisher)) {
+    throw new Error(`${publisher} is not a valid publisher`);
+  }
+
+  return heroes.filter((hero) => hero.publisher === publisher);
+};
